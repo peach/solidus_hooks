855f6bd684c17c4ec41d1d2391e19e28b888b427
Miguel Sancho <sanchocuba@gmail.com>
2017-10-19 19:48:17 -0700

    fixed association

--- app/models/notification_log.rb
+++ app/models/notification_log.rb
@@ -1,4 +1,4 @@
 class NotificationLog < ActiveRecord::Base
   belongs_to :event, class_name: Observer::Event.name
-  belongs_to :notification_hook, class_name: Hooks::NotificationHook
+  belongs_to :notification_hook, class_name: Hooks::NotificationHook.name, foreign_key: :notification_log_id
 end
